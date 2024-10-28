13c2de61cb1aba0cd385fc33161abfa2292e2392
NERRY <78302204+neri-2@users.noreply.github.com>
2024-10-29 00:46:23 +0900

    10/28 문제 풀이

--- src/baekjoon/silver/i/n1193/Main.java
+++ src/baekjoon/silver/i/n1193/Main.java
@@ -1,2 +1,30 @@
-package baekjoon.silver.i.n1193;public class Main {
+package baekjoon.silver.i.n1193;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // 입력
+        int X = Integer.parseInt(br.readLine());
+
+        int count = 1;
+
+        while (X > count) X -= count++;
+
+        if (count % 2 == 0) {
+            bw.write(X + "/" + (count - X + 1));
+        } else {
+            bw.write((count - X + 1) + "/" + X);
+        }
+
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
 }
